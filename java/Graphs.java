d77e4bcc760ec6bea26e09b55c72d74deb6627ff
bot-io <baivulcho@gmail.com>
2016-07-04 23:04:55 +0300

    Create Graphs.java

---
+++ java/Graphs.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+  private class Node{
+    public List<Node> children = new LinkedList<>();
+    public boolean visited = false;
+  }
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
