22880a7180587d8f75abedfd45adb07e277ade27
bot-io <baivulcho@gmail.com>
2016-07-05 15:16:19 +0300

    Update Breadth First Search: Shortest Reach.java

--- java/hackerrank/Breadth First Search: Shortest Reach.java
+++ java/hackerrank/Breadth First Search: Shortest Reach.java
@@ -2,8 +2,25 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
+    
+    private class Node{
+        public List<Node> children = new LinkedList<>();
+        public boolean visited = false;
+    }
+
+    
+    private static long solution(int[] a) {
+        long result = 0L;
+        return result;
+    }
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = in.nextInt();
+            int[] a = new int[n];
+            System.out.println(solution(a));
+        }
     }
 }
