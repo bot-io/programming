1607282a0fa2596ea646691f8c4359144661dd9c
bot-io <baivulcho@gmail.com>
2016-08-25 18:43:40 +0300

    Update FunnyString.java

--- java/hackerrank/FunnyString.java
+++ java/hackerrank/FunnyString.java
@@ -13,9 +13,18 @@ public class Solution {
         for(int i = 0; i < l/2; i++){
             char i1 = a.charAt(i);
             char i2 = a.charAt(i+1);
+            
+            char j1 = a.charAt(l-i-1);
+            char j2 = a.charAt(l-i-2);
+            
+            int diffDelta = Math.abs(i1-i2)-Math.abs(j1-j2);
+            
+            if(diffDelta != 0){
+                return not_funny;
+            }
         }
         
-        return a;
+        return funny;
     }
 
     public static void main(String[] args) {
