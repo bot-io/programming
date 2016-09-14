666f339b7438933d38768834927b7cbc56bbc0d8
bot-io <baivulcho@gmail.com>
2016-09-14 19:54:35 +0300

    Create Project Euler #1: Multiples of 3 and 5.java

---
+++ java/hackerrank/Project Euler #1: Multiples of 3 and 5.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    
+    private static long solution(int n) {
+        long result = 0L;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = in.nextInt();
+            System.out.println(solution(n));
+        }
+    }
+}
