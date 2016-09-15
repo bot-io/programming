dc8b0fcc6128ab20aa5340eece7b473c42508411
bot-io <baivulcho@gmail.com>
2016-09-15 12:37:17 +0300

    Update Project Euler #1: Multiples of 3 and 5.java

--- java/hackerrank/Project Euler #1: Multiples of 3 and 5.java
+++ java/hackerrank/Project Euler #1: Multiples of 3 and 5.java
@@ -3,9 +3,31 @@ import java.util.*;
 
 public class Solution {
     
-    private static long solution(int n) {
+    private static void solutionON(int n) {
         long result = 0L;
-        return result;
+        int candidate = 3;
+        int[] jx = {2,1,3,1,2,3,3};
+        int j = 0;
+        while(candidate<n){
+            //System.out.println("C: "+candidate);
+            result+=candidate;
+            candidate += jx[j];
+            j = (j+1)%7;
+        }
+        
+        System.out.println(result);
+    }
+    
+    private static void solutionO1(int num) {
+        long three,five,fifteen, result=0;
+        
+        three=(num-1)/3;
+        five=(num-1)/5;
+        fifteen=(num-1)/15;
+        
+        result = 3*(three*(three+1)/2)+5*(five*(five+1)/2)-15*(fifteen*(fifteen+1)/2);
+        
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
@@ -13,7 +35,8 @@ public class Solution {
         int t = in.nextInt();
         for (int i = 0; i < t; i++) {
             int n = in.nextInt();
-            System.out.println(solution(n));
+            solutionO1(n);
+            //System.out.println(solution(n));
         }
     }
 }
