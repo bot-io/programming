e7b8e563d419f5c411c3639c3287fbabda546c42
bot-io <baivulcho@gmail.com>
2016-09-17 15:01:33 +0300

    Update Matrix Layer Rotation.java

--- java/hackerrank/Matrix Layer Rotation.java
+++ java/hackerrank/Matrix Layer Rotation.java
@@ -2,8 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-
+    
+    public static void printm(int[][]a, int m, int n) {
+        for (int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                System.out.print(""+a[i][j]+" ");
+            }
+            System.out.print("\n");
+        }
+    }
+    
+    public static void printmSpirally(int[][]a, int m, int n) {
+        for (int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                System.out.print(""+a[i][j]+" ");
+            }
+            System.out.print("\n");
+        }
+    }
+    
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int m = in.nextInt();
+        int n = in.nextInt();
+        int r = in.nextInt();
+        int[][] a = new int[m][n];
+        for (int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                a[i][j] = in.nextInt();
+            }
+        }
+        printm(a, m, n);
     }
 }
