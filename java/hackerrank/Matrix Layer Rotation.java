5cf07bee2f9a0dd303cb58914e98cbe8f03057d9
bot-io <baivulcho@gmail.com>
2016-09-17 17:15:11 +0300

    Update Matrix Layer Rotation.java

--- java/hackerrank/Matrix Layer Rotation.java
+++ java/hackerrank/Matrix Layer Rotation.java
@@ -12,12 +12,39 @@ public class Solution {
         }
     }
     
+    public static void goDown(int[][]a, int x, int y, int maxM) {
+        for (int i = x; i < maxM; i++){
+            System.out.print(""+a[i][y]+" ");
+        }
+    }
+    
+    public static void goRight(int[][]a, int x, int y, int maxN) {
+        for (int j = y; j < maxN; j++){
+            System.out.print(""+a[x][j]+" ");
+        }
+    }
+    
+    public static void goUp(int[][]a, int x, int y, int minM) {
+        for (int i = x; i > minM; i--){
+            System.out.print(""+a[i][y]+" ");
+        }
+    }
+    
+    public static void goLeft(int[][]a, int x, int y, int minN) {
+        for (int j = y; j > minN; j--){
+            System.out.print(""+a[x][j]+" ");
+        }
+    }
+    
     public static void printmSpirally(int[][]a, int m, int n) {
-        for (int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                System.out.print(""+a[i][j]+" ");
-            }
-            System.out.print("\n");
+        int x=0,y=0;
+        while(x <= m/2 && y <= n/2){
+            goDown(a, x, y, m-1-x);
+            goRight(a, m-1-x, y, n-1-y);
+            goUp(a, m-1-x, n-1-y, y);
+            goLeft(a, x, n-1-y, x);
+            x++;
+            y++;
         }
     }
     
@@ -32,6 +59,6 @@ public class Solution {
                 a[i][j] = in.nextInt();
             }
         }
-        printm(a, m, n);
+        printmSpirally(a, m, n);
     }
 }
