e3df3fbea85990ebbe65cfae6c959d88c148abd4
Bot <baivulcho@gmail.com>
2017-03-19 19:55:36 +0200

    Merge branch 'master' of https://github.com/bot-io/programming
    # Conflicts:
    #	java/Algorithms/src/logger/Logger.java

--- java/Algorithms/src/logger/Logger.java
+++ java/Algorithms/src/logger/Logger.java
@@ -2,10 +2,6 @@ package logger;
 
 public class Logger {
 	public void log(String message) {
-<<<<<<< HEAD
 		System.out.println(message);
-=======
-		 System.out.println(message);
->>>>>>> 24b259e77ae6c3df5b586c3aef683eca7645bd3a
 	}
 }
